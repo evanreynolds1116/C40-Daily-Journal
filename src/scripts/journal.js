485994e560913452000cb32e9330e404bb1d5b3f
evan.m.reynolds95@gmail.com <evanr95@MSI.localdomain>
2020-04-22 15:12:03 -0500

    Daily Journal Part-2

---
+++ src/scripts/journal.js
@@ -0,0 +1,32 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const journalEntries = []
+
+const journalEntry = {
+  date: "April 15th",
+  concepts: "???",
+  entry: "Presented group projects",
+  mood: "Happy"
+}
+journalEntries.push(journalEntry)
+
+const journalEntry2 = {
+  date: "April 16th",
+  concepts: "JavaScript",
+  entry: "Basic JavaScript",
+  mood: "Happy"
+}
+journalEntries.push(journalEntry2)
+
+const journalEntry3 = {
+  date: "April 17th",
+  concepts: "JavaScript",
+  entry: "BRAIN FRIED",
+  mood: "Blegh"
+}
+journalEntries.push(journalEntry3)
+
+
+console.log(journalEntries)
